2e6a7b604fec7f45b273bde7ada6d8bbbac846f3
jkkariuki <jkkariuki15@gmail.com>
2023-05-22 15:47:47 -0400

    initial commit . full ecommerce project. no checkout. need addCartQuantity function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import Navigation from "./components/Navigation";
+import Home from "./Pages/Home";
+import Products from "./APICalls";
+import ProductCard from "./components/ProductCard";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ProductPage from "./Pages/ProductPage";
+
+import { useState, useEffect } from "react";
+import Footer from "./components/Footer";
+import ContactUs from "./Pages/Contact";
+
+const cartFromLocalStorage = JSON.parse(
+  localStorage.getItem("cartItems") || "[]"
+);
 function App() {
+  const [cartItems, setCartItems] = useState(cartFromLocalStorage);
+
+  useEffect(() => {
+    const json = JSON.stringify(cartItems);
+    localStorage.setItem("cartItems", json);
+  }, [cartItems]);
+
+  function onAdd(product) {
+    const exist = cartItems.find((item) => item.id === product.id);
+
+    if (exist) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id ? { ...exist, qty: exist.qty + 1 } : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+    console.log(cartItems);
+  }
+
+  function onRemove(product) {
+    const exist = cartItems.find((item) => item.id === product.id);
+
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((item) => item.id != product.id));
+    } else {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id ? { ...exist, qty: exist.qty - 1 } : item
+        )
+      );
+    }
+  }
+
+  function onDelete(product) {
+    const exist = cartItems.find((item) => item.id === product.id);
+
+    if (exist) {
+      setCartItems(cartItems.filter((item) => item.id != product.id));
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navigation
+          countCartItems={cartItems.length}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          cartItems={cartItems}
+          onDelete={onDelete}
+        />
+        <Routes>
+          <Route path="/" element={<Home onAdd={onAdd} />} />
+          <Route path="/products/:id" element={<ProductPage onAdd={onAdd} />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
